e2864a53ff8cbb9c09e99c0a74fd234894292589
mcsandovall <rasec_oiram@live.com.mx>
2023-03-02 20:52:10 -0600

    try and catch blocks

--- makeupMatcher/makeupApp/utils/color_correction.py
+++ makeupMatcher/makeupApp/utils/color_correction.py
@@ -1,3 +1,4 @@
+from msilib.schema import Error
 import cv2
 from PIL import Image
 import os, sys
@@ -11,10 +12,14 @@ GAMUT_MAPPIGN : int = 2;
 IMG_SHOW : int = 0;
 
 def correctImage(in_image : Image):
-    wbModel = wb_srgb.WBsRGB(gamut_mapping=GAMUT_MAPPIGN,upgraded=UPGRADED_MODEL);
-    Img = cv2.imread(in_image); # image read conver from PIL to cv2
-    outImg = wbModel.correctImage(Img);
-    return outImg;
+    try:
+        wbModel = wb_srgb.WBsRGB(gamut_mapping=GAMUT_MAPPIGN,upgraded=UPGRADED_MODEL);
+        Img = cv2.imread(in_image); # image read conver from PIL to cv2
+        outImg = wbModel.correctImage(Img);
+        return outImg;
+    except:
+        print("Error: Could not correct given image");
+        return in_image;
 
 
 
