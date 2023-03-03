a25187340ee1a5a83ed3af3116628dd70c145477
mcsandovall <rasec_oiram@live.com.mx>
2023-03-02 20:47:22 -0600

    correct image funct implemented

--- makeupMatcher/makeupApp/utils/color_correction.py
+++ makeupMatcher/makeupApp/utils/color_correction.py
@@ -6,7 +6,17 @@ sys.path.insert(1, '../../WB_sRGB/WB_sRGB_Python/')
 
 from classes import WBsRGB as wb_srgb
 
+UPGRADED_MODEL : int  = 1;
+GAMUT_MAPPIGN : int = 2;
+IMG_SHOW : int = 0;
+
 def correctImage(in_image : Image):
-    pass
+    wbModel = wb_srgb.WBsRGB(gamut_mapping=GAMUT_MAPPIGN,upgraded=UPGRADED_MODEL);
+    Img = cv2.imread(in_image); # image read conver from PIL to cv2
+    outImg = wbModel.correctImage(Img);
+    return outImg;
 
 
+
+if __name__ == '__main__':
+    print("pass");
