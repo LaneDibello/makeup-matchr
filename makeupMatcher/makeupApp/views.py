dde1ece80410d39613d916cb2aecd8050464d0dc
mcsandovall <rasec_oiram@live.com.mx>
2023-03-05 18:03:56 -0600

    more migration plus integration of html

--- makeupMatcher/makeupApp/views.py
+++ makeupMatcher/makeupApp/views.py
@@ -5,26 +5,6 @@ from makeupApp.models import Product
 # This is just a test that grabs the test product from the table
 
 def index(request):
-        
-
-        # Inserting Products:
-        # p = Product(name='django import test', vendor='Test', red=60, blue=70, green=90, url='TEST URL', brand='brandname haha', colorcode='17C')
-        # p.save()
-
-
-        #Selecting Values Example:
-        prod = Product.objects.get(name='django import test')
-        context = {
-            'name':prod.name,
-            'vendor':prod.vendor,
-            'brand':prod.brand,
-            'url':prod.url,
-            'red':prod.red,
-            'green':prod.green,
-            'blue':prod.blue,
-        }
-        return render(request, 'index.html', context)
-
-        return None
+    return render(request, 'index.html')
 
 
