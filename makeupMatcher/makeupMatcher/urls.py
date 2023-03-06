690c1346db2e20c1cfd1999e5f0a6944957949da
Michael Cadieux <mcadieux2023@tamu.edu>
2023-03-06 13:08:55 -0600

    added names to url.py, fixed navbar routing

--- makeupMatcher/makeupMatcher/urls.py
+++ makeupMatcher/makeupMatcher/urls.py
@@ -18,9 +18,9 @@ from django.urls import path, re_path
 from makeupApp import views
 
 urlpatterns = [
-    path('admin/', admin.site.urls),
-    path("", views.index),
-    path('about/', views.about),
-    path('test/', views.test),
-    path('results/', views.results),
+    path('admin/', admin.site.urls, name="admin"),
+    path("", views.index, name="index"),
+    path('about/', views.about, name="about"),
+    path('test/', views.test, name="test"),
+    path('results/', views.results, name="results"),
 ]
