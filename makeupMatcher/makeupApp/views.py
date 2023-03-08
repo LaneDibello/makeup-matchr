516b0fd095a53042314a20ac5f0c8efb80629286
Lane <lane.dibello@gmail.com>
2023-03-08 14:03:48 -0600

    K nearest matches working

--- makeupMatcher/makeupApp/views.py
+++ makeupMatcher/makeupApp/views.py
@@ -1,9 +1,6 @@
 from django.shortcuts import render
 from makeupApp.models import Product
 
-# Gutted by the old firebase stuff 
-# This is just a test that grabs the test product from the table
-
 def index(request):
     return render(request, 'index.html')
 
