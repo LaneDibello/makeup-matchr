ac055675033fd883e8736e0eb0c5e8efe9530d69
Lane <lane.dibello@gmail.com>
2023-03-22 12:40:02 -0500

    Test Color Picker

--- makeupMatcher/makeupApp/views.py
+++ makeupMatcher/makeupApp/views.py
@@ -1,5 +1,7 @@
 from django.shortcuts import render
 from makeupApp.models import Product
+import re
+from PIL import Image
 
 def index(request):
     return render(request, 'index.html')
@@ -8,10 +10,19 @@ def about(request):
     return render(request, 'about.html')
 
 def test(request):
-    query_results = Product.objects.all()[:20]
+    coords_s = request.META['QUERY_STRING']
+    coords = [0,0]
+    coords = list(map(int, re.findall(r'\d+', coords_s)))
+    im = Image.open('DELETE_ME.jpg').load()
+    color = im[coords[0], coords[1]]
     context = {
-        'query_results':query_results,
+        'x': coords[0],
+        'y': coords[1],
+        'r': color[0],
+        'g': color[1],
+        'b': color[2],
     }
+
     return render(request, 'testing.html', context)
 
 def results(request):
