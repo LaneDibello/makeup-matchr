761b3baf5762fb099d501b1699c490f75a9573d6
bcarl <bcarl@tamu.edu>
2023-03-25 02:52:12 -0500

    Fixed various bugs

--- scraping/sephora.py
+++ scraping/sephora.py
@@ -7,17 +7,22 @@ if __name__ == '__main__':
         'vendor': 'Sephora',
         'next': (By.CLASS_NAME, 'css-bk5oor'),
         'product': (By.CLASS_NAME, 'css-klx76'),
-        'swatch': (By.XPATH, "//button[@data-at='swatch']"),
+        'swatch': (By.XPATH, "//button[@aria-describedby='colorSwatch']"),
         'click': True,
         'brand': (By.XPATH, "//a[@data-at='brand_name']"),
         'name': (By.XPATH, "//span[@data-at='product_name']"),
         'code': (By.XPATH, "//div[@data-at='sku_name_label']/span"),
         'code_attribute': None,
-        'img': (By.XPATH, "//div/img[contains(@src, 'https://www.sephora.com/productimages/sku/')]"),
+        'img': (By.XPATH, "//button[@data-at='selected_swatch']/div/img"),
         'price': (By.XPATH, "//p[@data-comp='Price ']/span/span[1]/b")
     }
 
     test: Scraper = Scraper(args)
-    test.scrape()
+    items = test.scrape_product('https://www.sephora.com/product/it-cosmetics-your-skin-but-better-foundation-skincare-P461600')
+    for item in items:
+        print(item)
+    
+    print(len(items))
+    # test.scrape(4)
 
-    test.to_tsv()
+    # test.to_tsv()
