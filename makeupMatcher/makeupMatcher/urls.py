b1d14c5a7678067f2c98267f1e9633dab98f7208
mcsandovall <rasec_oiram@live.com.mx>
2023-03-27 23:43:11 -0500

    deletion of images after window close

--- makeupMatcher/makeupMatcher/urls.py
+++ makeupMatcher/makeupMatcher/urls.py
@@ -25,5 +25,6 @@ urlpatterns = [
     path('about/', views.about, name="about"),
     path('test/', views.test, name="test"),
     path('results/', views.results, name="results"),
-    path('picker/', views.picker, name="picker")
+    path('picker/', views.picker, name="picker"),
+    path('browser_closed/', views.browser_closed, name='browser_closed'),
 ] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
