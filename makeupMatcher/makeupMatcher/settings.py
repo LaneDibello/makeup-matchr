102d91542515fbd878f9b6c41106bfed117c0f5e
bcarl <bcarl@tamu.edu>
2023-04-29 18:21:08 -0500

    Session may have not been working

--- makeupMatcher/makeupMatcher/settings.py
+++ makeupMatcher/makeupMatcher/settings.py
@@ -41,7 +41,8 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
-    'makeupApp',
+    'django.contrib.sessions'
+    'makeupApp'
 ]
 
 MIDDLEWARE = [
@@ -134,3 +135,5 @@ STATICFILES_STORAGE = 'django.contrib.staticfiles.storage.StaticFilesStorage'
 # https://docs.djangoproject.com/en/4.1/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+
+SESSION_ENGINE = 'django.contrib.sessions.backend.signed_cookies'
