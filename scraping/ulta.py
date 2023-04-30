146b189e91123340fcdef8078e059edaa3440742
Bennett <bennettacarley@gmail.com>
2023-04-30 17:01:25 -0500

    Scraping (#101)
    * Working on ulta
    
    * Changed price extraction to regex
    
    * Saved images on error
    
    * updated ulta:

---
+++ scraping/ulta.py
@@ -0,0 +1,26 @@
+from scraper import *
+
+if __name__ == '__main__':
+    args: dict = {
+        'limit': 5,
+        'base': 'https://www.ulta.com/shop/makeup/face/foundation',
+        'vendor': 'Ulta',
+        'next': (By.XPATH, "//div[@class='LoadContent']/button"),
+        'product': (By.XPATH, "//div[@class='ProductCard']/a"),
+        'swatch': (By.XPATH, "//div[@class='ProductSwatches']//span[@class='Swatch__image']"),
+        'click': True,
+        'brand': (By.XPATH, "//div[@class='ProductInformation']/h1/span[1]/a"),
+        'name': (By.XPATH, "//div[@class='ProductInformation']/h1/span[2]"),
+        'code': (By.XPATH, "//div[@class='SwatchDropDown__nameDescription']/span"),
+        'code_attribute': None,
+        'img': (By.XPATH, "//div[@class='ProductSwatches']//button[@aria-current='true']/span/img"),
+        'price': (By.XPATH, "//div[@class='ProductHero__content']/div[@class='ProductPricing']/span")
+    }
+
+    test: Scraper = Scraper(args)
+    test.scrape()
+    # products = test.scrape_product('https://www.ulta.com/p/ultra-cc-cream-radiant-foundation-with-100-physical-broad-spectrum-spf-17-pimprod2026038')
+    # for product in products:
+    #     print(product)
+
+    test.to_tsv()
